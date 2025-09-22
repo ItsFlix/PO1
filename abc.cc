4e7a0cd10324d619cc694701ce66f747c1859838
ItsFlix <66955402+ItsFlix@users.noreply.github.com>
2025-09-22 14:15:32 +0200

    Create abc.cc

---
+++ abc.cc
@@ -0,0 +1,18 @@
+
+    //abc formule
+    int a = rand() % 20;
+    int b = rand() % 20;
+    int c = rand() % 20;
+
+    if (a == 0) {
+        a++;
+    }
+    else if (b == 0) {
+        b++;
+    }
+    else if (c == 0) {
+        c++;
+    }
+
+    // a x2 + b x + c = 0
+    //cout << "equation: " << a << "x^2" << '+' << b << 'x' << '+' << c << " = 0";
